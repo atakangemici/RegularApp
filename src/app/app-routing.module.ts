b2b42d9ab37d1ac8a805048ac92dff20dad355ff
Atakan Gemici <atakan.gemici@ofisim.com>
2020-12-01 23:08:19 +0300

    bugfix.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", component: HomeComponent }
+  { path: "", pathMatch: "full", component: HomeComponent },
+  { path: "topmenu", component: TopMenuComponent }
 
 ];
 
